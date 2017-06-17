afd164a9b98bcc531ce8c99df266b9884dbd607a
ArtemisD <dianah456@gmail.com>
2017-06-17 14:44:18 -0700

    refacotred findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -15,20 +15,89 @@
 
 
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+// window.findNRooksSolution = function(n) {
+//   //create a new board
+//   var solution = new Board({n: n});
+//   //use nested for loops to go through every coordinate on the board
+//   for (var x = 0; x < n; x++) {
+//     for (var y = 0; y < n; y++) {
+//       //at each coordinte, toggle the piece to 1
+//       solution.togglePiece(x, y);
+//       //if when you toggle that piece, it creates a rooks conflict on the board
+//       if (solution.hasAnyRooksConflicts()) {
+//         //toggle that piece back to 0 to get rid of conflict
+//         solution.togglePiece(x, y);
+//
+//       }
+//     }
+//   }
+//   for (var i = 0; i < n; i++) {
+//     console.log(solution.rows()[i]);
+//   }
+//   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+//   return solution.rows();
+// };
 
+window.findNRooksSolution = function(n) {
+  //create a new board
+  var solution = new Board({n: n});
+  //recursive function
+  var callback = function(row) {
+    //base case: stop the search when row is equal to n
+    if (row === n) {
+      return solution;
+    }
+    //iterate through n staring at the first column
+    for (var colIndex = 0; colIndex < n; colIndex++) {
+      //toggle the piece at that coordinate
+      solution.togglePiece(row, colIndex);
+      //check if toggling that piece creates any rooks conflicts
+      if (solution.hasAnyRooksConflicts()) {
+        //if it does, toggle taht piece back
+        solution.togglePiece(row, colIndex);
+        //else call inner recursive function on the next row
+      } else {
+        callback(row + 1);
+      }
+    }
+
+  };
+  //call recursive function on row 0
+  callback(0);
+
+  for (var i = 0; i < n; i++) {
+    console.log(solution.rows()[i]);
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
 
+    // create a new board of zeros
+
+    // instaniate a function / sub-routine, pass in the board
+
+      // establish a base case that once i hit n, increase solution count
+
+      // else iterate through columns looking for place w/o conflict
+
+        // place piece and check for conflicts
+
+          // if conflict, do not toggle and continue iteration
+
+          // if not conflict then advance the...
+
+
+
+
+
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
+
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
@@ -45,3 +114,10 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+
+var b = new Board({n:4});
+b.togglePiece(0,0);
+b.togglePiece(0,1);
+console.log(b.hasAnyRooksConflicts());
